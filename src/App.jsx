9658b9637356e62637a2e758199afd47670a4374
Adarsh Gupta <adarshgupta.1312@gmail.com>
2026-02-05 15:39:18 +0530

    Restructure the coe

---
+++ src/App.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import axios from "axios";
+import "./App.css";
+
+function App() {
+  const [city, setCity] = useState("");
+  const [weather, setWeather] = useState(null);
+
+  const API_KEY = "a47a9ff6c3b531e564c820e483503ea5";
+
+  const getWeather = async () => {
+    if (!city) return;
+
+    try {
+      const response = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`,
+      );
+      setWeather(response.data);
+    } catch (error) {
+      alert("City not found");
+    }
+  };
+
+  return (
+    <div className="app">
+      <h1>Weather App</h1>
+
+      <div className="search-box">
+        <input
+          type="text"
+          placeholder="Enter city name"
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
+        />
+        <button onClick={getWeather}>Search</button>
+      </div>
+
+      {weather && (
+        <div className="weather-card">
+          <h2>{weather.name}</h2>
+          <p>{weather.weather[0].main}</p>
+          <h3>{weather.main.temp}°C</h3>
+          <p>Humidity: {weather.main.humidity}%</p>
+          <p>Wind: {weather.wind.speed} m/s</p>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
